2364d4c9e12e129530f220228d8a3338c7690124
bernardo <bernardo.cauich.reyna@gmail.com>
2019-08-21 00:17:41 -0500

    Modulo productos

--- system/cms/themes/bootstrap/js/scripts.js
+++ system/cms/themes/bootstrap/js/scripts.js
@@ -72,42 +72,7 @@ jQuery(function($) {
 	 */
 	pyro.init_autocomplete_search = function(){
 		var cache = {}, lastXhr;
-		$(".search-query").autocomplete({
-			minLength: 2,
-			delay: 250,
-			source: function( request, response ) {
-				var term = request.term;
-				if ( term in cache ) {
-					response( cache[ term ] );
-					return;
-				}
-				lastXhr = $.getJSON(SITE_URL + 'admin/search/ajax_autocomplete', request, function( data, status, xhr ) {
-					cache[ term ] = data.results;
-					if ( xhr === lastXhr ) {
-						response( data.results );
-					}
-				});
-			},
-			
-			open: function (event, ui) {
-				$(this).data("autocomplete").menu.element.addClass("search-results animated-zing dropDown");
-			},
-			
-			focus: function(event, ui) {
-				// $("#searchform").val( ui.item.label);
-				return false;
-			},
-			select: function(event, ui) {
-				window.location.href = ui.item.url;
-				return false;
-			}
-		})
-		.data("autocomplete")._renderItem = function(ul, item){
-			return $("<li></li>")
-			.data("item.autocomplete", item)
-			.append('<a href="' + item.url + '">' + '<span>' + item.title + '</span>' + '<div class="keywords">' + item.keywords + '</div><div class="singular">' + item.singular + '</div>' + '</a>')
-			.appendTo(ul);
-		};
+		
 	};
 
 	/**
@@ -138,24 +103,18 @@ jQuery(function($) {
   			window.location = $(this).find("option:selected").val();
 		});
 
-		$('.topbar ul li:not(#dashboard-link)').hoverIntent({
-			sensitivity: 7,
-			interval: 75,
-			over: function(){ $(this).find('ul:first:hidden').css({visibility: "visible", display: "none"}).slideDown(200) },
-			timeout: 0,
-			out: function(){ $(this).parent().find('ul').slideUp(200) }
-		});
+	
 
 		// Add class to dropdowns for styling
 		$(".topbar ul li:has(ul)").children("a").addClass("menu");
 
 		// Add the close link to all alert boxes
-		$('.alert').livequery(function(){
-			$(this).prepend('<a href="#" class="close"></a>');
-		});
+		//$('.alert').livequery(function(){
+		//	$(this).prepend('<a href="#" class="close"></a>');
+		//});
 
 		// Close the notifications when the close link is clicked
-		$('a.close').live('click', function(e){
+		$('a.close').on('click', function(e){
 			e.preventDefault();
 			$(this).fadeTo(200, 0); // This is a hack so that the close link fades out in IE
 			$(this).parent().fadeTo(200, 0);
@@ -165,17 +124,17 @@ jQuery(function($) {
 			});
 		});
 
-		$("#datepicker, .datepicker").datepicker({dateFormat: 'yy-mm-dd'});
+		//$("#datepicker, .datepicker").datepicker({dateFormat: 'yy-mm-dd'});
 
 		// Fade in the notifications
-		$('.alert').livequery(function(){
-			$(this).fadeIn('slow', function(){
-				$(window).trigger('notification-complete');
-			});
-		});
+	//	$('.alert').livequery(function(){
+		//	$(this).fadeIn('slow', function(){
+		//		$(window).trigger('notification-complete');
+		//	});
+		//});
 
 		// Check all checkboxes in container table or grid
-		$(".check-all").live('click', function () {
+		$(".check-all").on('click', function () {
 			var check_all		= $(this),
 				all_checkbox	= $(this).is('.grid-check-all')
 					? $(this).parents(".list-items").find(".grid input[type='checkbox']")
@@ -200,7 +159,7 @@ jQuery(function($) {
 		$(".table_action_buttons .btn").prop('disabled', true);
 
 		// Enable/Disable table action buttons
-		$('input[name="action_to[]"], .check-all').live('click', function () {
+		$('input[name="action_to[]"], .check-all').on('click', function () {
 
 			if( $('input[name="action_to[]"]:checked, .check-all:checked').length >= 1 ){
 				$(".table_action_buttons .btn").prop('disabled', false);
@@ -210,7 +169,7 @@ jQuery(function($) {
 		});
 
 		// Confirmation
-		$('a.confirm').live('click', function(e){
+		$('a.confirm').on('click', function(e){
 			e.preventDefault();
 
 			var href		= $(this).attr('href'),
@@ -229,7 +188,7 @@ jQuery(function($) {
 		});
 
 		//use a confirm dialog on "delete many" buttons
-		$(':submit.confirm').live('click', function(e, confirmation){
+		$(':submit.confirm').on('click', function(e, confirmation){
 
 			if (confirmation)
 			{
@@ -254,62 +213,53 @@ jQuery(function($) {
 		});
 
 		// Table zerbra striping
-		$("tbody tr:nth-child(even)").livequery(function () {
-			$(this).addClass("alt");
-		});
+		//$("tbody tr:nth-child(even)").livequery(function () {
+		//	$(this).addClass("alt");
+		//});
 
 
-		$('#main, .tabs').livequery(function () {
-			$(this).tabs();
-			$(this).tabs('paging', { cycle: true, follow: false } );
-		});
+		//$('#main, .tabs').livequery(function () {
+		//	$(this).tabs();
+		//	$(this).tabs('paging', { cycle: true, follow: false } );
+		//});
 
-		$('#tabs').livequery(function () {
-			$(this).tabs({
+	//	$('#tabs').livequery(function () {
+		//	$(this).tabs({
 				// This allows for the Back button to work.
-				select: function(event, ui) {
-					parent.location.hash = ui.tab.hash;
-				},
-				load: function(event, ui) {
-					confirm_links();
-					confirm_buttons();
-				}
-			});
-		});
+			//	select: function(event, ui) {
+			//		parent.location.hash = ui.tab.hash;
+			//	},
+			//	load: function(event, ui) {
+			//		confirm_links();
+				//	confirm_buttons();
+			//	}
+		//	});
+		//});
 
 		var current_module = $('#page-header h1 a').text();
 		// Colorbox modal window
-		$('a[rel=modal], a.modal').livequery(function() {
-			$(this).colorbox({
-				width: "60%",
-				maxHeight: "90%",
-				current: current_module + " {current} / {total}",
-				onComplete: function(){ pyro.chosen() }
-			});
-		});
+		//$('a[rel=modal], a.modal').livequery(function() {
+		//	$(this).colorbox({
+		//		width: "60%",
+		//		maxHeight: "90%",
+		//		current: current_module + " {current} / {total}",
+			//	onComplete: function(){ pyro.chosen() }
+		//	});
+		//});
 
-		$('a[data-inline-modal]').livequery(function() {
-			var element_id = $(this).attr('data-inline-modal');
-			$(this).colorbox({
-				width: "60%",
-				maxHeight: "90%",
-				inline: true,
-				href: element_id,
-				current: current_module + " {current} / {total}",
-				onComplete: function(){ pyro.chosen() }
-			});
-		});
+	//	$('a[data-inline-modal]').livequery(function() {
+		//	var element_id = $(this).attr('data-inline-modal');
+		//	$(this).colorbox({
+		//		width: "60%",
+		//		maxHeight: "90%",
+		//		inline: true,
+		//		href: element_id,
+			//	current: current_module + " {current} / {total}",
+		//		onComplete: function(){ pyro.chosen() }
+		//	});
+	//	});
 
-		$('a[rel="modal-large"], a.modal-large').livequery(function() {
-			$(this).colorbox({
-				width: "90%",
-				height: "95%",
-				iframe: true,
-				scrolling: false,
-				current: current_module + " {current} / {total}",
-				onComplete: function(){ pyro.chosen() }
-			});
-		});
+		
 	};
 
 	pyro.clear_notifications = function()
@@ -440,22 +390,7 @@ jQuery(function($) {
 		if( ! pyro.is_mobile ){
 
 			// Chosen
-			$('select:not(.skip)').livequery(function(){
-				$(this).addClass('chzn').addClass('hidden').trigger("liszt:updated");
-				$(".chzn").chosen();
-
-				// This is a workaround for Chosen's visibility bug. In short if a select
-				// is inside a hidden element Chosen sets the width to 0. This iterates through
-				// the 0 width selects and sets a fixed width.
-				$('.chzn-container').each(function(i, ele){
-					if ($(ele).width() == 0) {
-						$(ele).css('width', '236px');
-						$(ele).find('.chzn-drop').css('width', '234px');
-						$(ele).find('.chzn-search input').css('width', '200px');
-						$(ele).find('.search-field input').css('width', '225px');
-					}
-				});
-			});
+			
 		}
 	}
 
@@ -481,10 +416,10 @@ jQuery(function($) {
 	});
 
 	//close colorbox only when cancel button is clicked
-	$('#cboxLoadedContent a.cancel').live('click', function(e) {
+	/*$('#cboxLoadedContent a.cancel').on('click', function(e) {
 		e.preventDefault();
 		$.colorbox.close();
-	});
+	});*/
 
 
 	// Title toggle
@@ -493,68 +428,17 @@ jQuery(function($) {
 	});
 
 	// Draggable / Droppable
-	$("#sortable").sortable({
+	/*$("#sortable").sortable({
 		placeholder : 'dropzone',
 	    handle : '.draggable',
 	    update : function () {
 	      var order = $('#sortable').sortable('serialize');
 	    }
-	});
+	});*/
 
 	// Pretty Photo
 	$('#main a:has(img)').addClass('prettyPhoto');
-	$("a[class^='prettyPhoto']").prettyPhoto();
+	//$("a[class^='prettyPhoto']").prettyPhoto();
 
-	// Tipsy
-	$('.tooltip').tipsy({
-		gravity: $.fn.tipsy.autoNS,
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-s').tipsy({
-		gravity: 's',
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-e').tipsy({
-		gravity: 'e',
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-w').tipsy({
-		gravity: 'w',
-		fade: true,
-		html: true
-	});
-
-	//functions for codemirror
-	$('.html_editor').each(function() {
-		CodeMirror.fromTextArea(this, {
-		    mode: 'text/html',
-		    tabMode: 'indent',
-			height : '500px',
-			width : '500px',
-		});
-	});
-
-	$('.css_editor').each(function() {
-		CodeMirror.fromTextArea(this, {
-		    mode: 'css',
-		    tabMode: 'indent',
-			height : '500px',
-			width : '500px',
-		});
-	});
-
-	$('.js_editor').each(function() {
-		CodeMirror.fromTextArea(this, {
-		    mode: 'javascript',
-		    tabMode: 'indent',
-			height : '500px',
-			width : '500px',
-		});
-	});
+	
 });
